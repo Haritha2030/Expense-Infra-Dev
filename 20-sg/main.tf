a76b50dde819594f258883cc55e40389c0fddf5f
Haritha <harithanallagadda4905@gmail.com>
2024-12-25 08:45:28 +0530

    sg

--- 20-sg/main.tf
+++ 20-sg/main.tf
@@ -1,5 +1,5 @@
 module "mysql_sg"{
-    source = "../../terraform-aws-security-group"
+    source = "../../Terraform-Aws-Security-Group"
     project_name = var.project_name
     environment = var.environment
     sg_name = "mysql"
@@ -8,12 +8,87 @@ module "mysql_sg"{
     sg_tags = var.mysql_sg_tags
 }
 
-module "mysql_sg"{
-    source = "../../terraform-aws-security-group"
+module "backend_sg"{
+    source = "../../Terraform-Aws-Security-Group"
     project_name = var.project_name
     environment = var.environment
-    sg_name = "mysql"
+    sg_name = "backend"
     vpc_id = local.vpc_id
     common_tags = var.common_tags
-    sg_tags = var.mysql_sg_tags
+    sg_tags = var.backend_sg_tags
+}
+
+module "frontend_sg"{
+    source = "../../Terraform-Aws-Security-Group"
+    project_name = var.project_name
+    environment = var.environment
+    sg_name = "frontend"
+    vpc_id = local.vpc_id
+    common_tags = var.common_tags
+    sg_tags = var.frontend_sg_tags
+}
+
+module "bastion_sg"{
+    source = "../../Terraform-Aws-Security-Group"
+    project_name = var.project_name
+    environment = var.environment
+    sg_name = "bastion"
+    vpc_id = local.vpc_id
+    common_tags = var.common_tags
+    sg_tags = var.bastion_sg_tags
+}
+
+# MySQL allowing connection on 3306 from the instances attached to Backend sg
+resource "aws_security_group_rule" "mysql_backend" {
+  type              = "ingress"
+  from_port         = 3306
+  to_port           = 3306
+  protocol          = "tcp"
+  source_security_group_id      = module.backend_sg.id
+  security_group_id =  module.mysql_sg.id
+}
+
+resource "aws_security_group_rule" "backend_frontend" {
+  type              = "ingress"
+  from_port         = 8080
+  to_port           = 8080
+  protocol          = "tcp"
+  source_security_group_id      = module.frontend_sg.id
+  security_group_id =  module.backend_sg.id
+}
+
+resource "aws_security_group_rule" "frontend_public" {
+  type              = "ingress"
+  from_port         = 80
+  to_port           = 80
+  protocol          = "tcp"
+  cidr_blocks       = ["0.0.0.0/0"]
+  security_group_id = module.frontend_sg.id
+}
+
+resource "aws_security_group_rule" "mysql_bastion" {
+  type              = "ingress"
+  from_port         = 22
+  to_port           = 22
+  protocol          = "tcp"
+  source_security_group_id      = module.bastion_sg.id
+  security_group_id =  module.mysql_sg.id
+}
+
+resource "aws_security_group_rule" "backend_bastion" {
+  type              = "ingress"
+  from_port         = 22
+  to_port           = 22
+  protocol          = "tcp"
+  source_security_group_id      = module.bastion_sg.id
+  security_group_id =  module.backend_sg.id
+}
+
+resource "aws_security_group_rule" "fronend_bastion" {
+  type              = "ingress"
+  from_port         = 22
+  to_port           = 22
+  protocol          = "tcp"
+  source_security_group_id      = module.bastion_sg.id
+  security_group_id =  module.frontend_sg.id
 }
